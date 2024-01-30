c72f9def232d95a607d3546059de4e32112b8dd7
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-30 20:25:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let value1 = parseFloat(prompt("Enter the value1"));
+let value2 = value1*2;
+let value3 = parseFloat(prompt("Enter the value3"));
+let result = value2 + value3;
+alert("The final result is: " + result);
